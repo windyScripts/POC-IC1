3d776e1ab9486fe65beb5b8b82e9d9207574f502
Aravind U Shenoy <aravindushenoy@Aravinds-MacBook-Air.local>
2025-01-02 12:47:23 +0530

    updates clickhouse config

--- clickhouse/src/config/clickhouse-config.ts
+++ clickhouse/src/config/clickhouse-config.ts
@@ -3,7 +3,7 @@ import { createClient } from '@clickhouse/client' // or '@clickhouse/client-web'
 export const cHClient = createClient({
   /* configuration */
   url:"http://localhost:8123",
-  username:"default",
-  password:"",
-  database:""
+  username:"clickHouse",
+  password:"clickHouse",
+  database:"clickHouse"
 })
